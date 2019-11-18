f8c0176f1f2db1e546ea840ce483b82f2275c720
Eniko Zsoldos <enikozsoldos93@gmail.com>
2019-11-18 18:45:47 +0200

    Add browsers

---
+++ src/test/java/utils/WebBrowsers.java
@@ -0,0 +1,32 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class WebBrowsers {
+
+    public static WebDriver getDriver(Browsers browser){
+        WebDriver driver = null;
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-fullscreen", "start-maximized");
+
+        switch(browser) {
+            case CHROME:
+                System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
+                driver = new ChromeDriver(chromeOptions);
+                break;
+            case FIREFOX:
+                System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver");
+                driver = new FirefoxDriver();
+                break;
+            case SAFARI:
+                driver = new SafariDriver();
+                break;
+        }
+        return driver;
+    }
+}
